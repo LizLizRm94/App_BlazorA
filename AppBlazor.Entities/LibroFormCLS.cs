a75fc7a1728addaf4d7cf8f6c105fd8ea2176aa0
Alumno <Alumno@b403-18>
2025-08-11 12:23:28 -0400

    Agregar archivos de proyecto.

---
+++ AppBlazor.Entities/LibroFormCLS.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AppBlazor.Entities
+{
+    public class LibroFormCLS
+    {
+        [Required (ErrorMessage = "EL id es requerido")]
+        [Range(1,int.MaxValue, ErrorMessage ="El valor debe ser positivo")]
+        public int idLibro { get; set; }
+        [Required(ErrorMessage = "EL titulo es requerido")]
+        [MaxLength (100, ErrorMessage ="La longitud maxima es de 100 caracteres")]
+        public string titulo { get; set; } = null!;
+        [Required(ErrorMessage = "EL resumen es requerido")]
+        [MinLength (20, ErrorMessage ="La longitud minima es de 20 caracteres")]
+        public string resumen { get; set; } = string.Empty;
+
+        
+    }
+}
